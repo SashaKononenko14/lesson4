6b20e720732ffcf619526c24534dfca4a0ce76c1
student <kononenkos660@gmail.com>
2023-12-07 17:17:47 +0200

    середина

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-class Parent:
+#Наслідування класів
+class Grandparents:
+    height = 170
+    eyes = "green"
+    age = 70
+class Parent(Grandparents):
     pass
+    age = 40
 class Child(Parent):
     pass
+    age = 15
+    height = 165
+    def __init__(self):
+        print(self.height)
+        print(self.eyes)
+        print(self.age)
+
+max = Child()
+#приклад
 class Human:
-    pass
+    height = 170
 class Student(Human):
     pass
+class Worker(Human):
+    pass
+
+nick = Student()
+kate = Worker()
+print(nick.height, kate.height)
